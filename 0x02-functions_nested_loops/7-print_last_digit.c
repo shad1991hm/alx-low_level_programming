6f4e715beb29bb2a0f393abf0e6bf53c4bae96dd
Sidrak <shad1991hm@gmail.com>
2022-06-07 15:24:30 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit
+ * @pld: digits to calculate
+ * Return: last digit;
+ */
+int print_last_digit(int pld)
+{
+	return (pld % 10);
+}
