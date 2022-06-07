bf405ba8a775689b61bd368f487d42c5c98d884f
Sidrak <shad1991hm@gmail.com>
2022-06-07 14:05:08 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - check alphabet
+ * @n: the number to be checked
+ * Return: 1 or 0;
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
