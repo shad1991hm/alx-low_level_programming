f0df75af5daceb076bb133d16db601643b950dfe
Sidrak <shad1991hm@gmail.com>
2022-06-07 03:33:07 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+ * main - Start here
+ * Return: Always 0
+ */
+int main(void)
+{
+	char pc[9] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		putchar(pc[i]);
+	}
+	putchar('\n');
+	return (0);
+}
