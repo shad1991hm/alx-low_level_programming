f24a2b3c5147ea2f178f94ffa3486a581b82d9a8
Sidrak <shad1991hm@gmail.com>
2022-06-07 13:52:02 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "mian.h"
+
+/**
+ * _isalpha - check alphabet
+ * @c: the alphabet to be checked
+ * Return: 1 or 0;
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+		return (1);
+	else
+		return (0);
+}
