1b31fc006ab4416acd8ba859a922b4ee89594ff1
Sidrak <shad1991hm@gmail.com>
2022-06-07 17:16:42 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,7 +26,7 @@ void times_table(void)
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
 			}
-			else if ((product < 10) && (y != 0))
+			else if ((product < 10) && (b != 0))
 			{
 				_putchar(' ');
 				_putchar((product % 10) + '0');
