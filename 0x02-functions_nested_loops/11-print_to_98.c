233217bfd5bb8e8047a2017471af961e3a9ae0f7
Sidrak <shad1991hm@gmail.com>
2022-06-07 17:43:32 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print the 9 times table
+ * @n: integer value
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n = n; n <= 97; n++)
+			printf("%d, ", n);
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+	}
+	_putchar("98\n");
+}
