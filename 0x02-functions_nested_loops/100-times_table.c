79dfec02eeaf5dc7d41483077c8f23ab68f22c57
Sidrak <shad1991hm@gmail.com>
2022-06-08 08:16:47 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,39 +8,36 @@ void print_times_table(int n)
 {
 	int a, b, p;
 
-	if (n < 15 && n >= 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (a = 0; a <= n; a++)
 		{
 			for (b = 0; b <= n; b++)
 			{
 				p = a * b;
-				if (p > 99)
+				if (b == 0)
+					_putchar(k + '0');
+				else if (p < 10 && b != 0)
 				{
 					_putchar(',');
-					_putchar(32);
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+				else if (p >= 10 && p < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(((p / 10) + '0');
+					_putchar((p % 10) + '0');
+				}
+				else if (p >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar((p / 100) + '0');
 					_putchar(((p / 10) % 10) + '0');
 					_putchar((p % 10) + '0');
-				}
-				else if (p > 9)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar((p / 100) + '0');
-					_putchar(((p / 10) % 10) + '0');
-					_putchar((p % 10) + '0');
-				}
-				else
-				{
-					if (b != 0)
-					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(p + '0');
 				}
 			}
 		_putchar('\n');
