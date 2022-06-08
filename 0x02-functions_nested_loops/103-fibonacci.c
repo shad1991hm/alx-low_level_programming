78b39d71feb9cd0c5dd6dddc4b453c1c6d3d0e7f
Sidrak <shad1991hm@gmail.com>
2022-06-07 21:09:42 -0700

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int a = 0;
-	long b = 1, c =2, sum =c
+	long b = 1, c =2, sum =c;
 
 	while (c + b < 4000000)
 	{
@@ -16,9 +16,9 @@ int main(void)
 
 		if (c % 2 == 0)
 			sum += c;
-		b = c -b;
+		b = c - b;
 		a++;
 	}
 	printf("%ld'n", sum);
-	retrun (0);
+	return (0);
 }
