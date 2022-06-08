ee60455f2871fe3829f037d615b0910c0bf190a7
Sidrak <shad1991hm@gmail.com>
2022-06-07 21:04:33 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the sum of even fibonacci less than 4000000
+ * Return: Always 0;
+ */
+int main(void)
+{
+	int a = 0;
+	long b = 1, c =2, sum =c
+
+	while (c + b < 4000000)
+	{
+		c += j;
+
+		if (c % 2 == 0)
+			sum += c;
+		b = c -b;
+		a++;
+	}
+	printf("%ld'n", sum);
+	retrun (0);
+}
