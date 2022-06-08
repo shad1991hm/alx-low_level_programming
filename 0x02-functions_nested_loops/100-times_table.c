7adb1d749e7010b42fa6233cca86abbb5fbde938
Sidrak <shad1991hm@gmail.com>
2022-06-07 19:52:58 -0700

    Task 14

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,14 +8,14 @@ void print_times_table(int n)
 {
 	int a, b, product;
 
-	if (n < 15 && n > 0)
+	if (n < 15 && n >= 0)
 	{
 		for (a = 0; a <= n; a++)
 		{
 			for (b = 0; b <= n; b++)
 			{
 				product = a * b;
-				if (b != 0)
+				if (product > 99)
 				{
 					_putchar(',');
 					_putchar(' ');
