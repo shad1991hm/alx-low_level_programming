e2e5fa381e38b9387377ab24b88e021f952df99c
Sidrak <shad1991hm@gmail.com>
2022-06-13 01:34:29 -0700

    $message

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp
+	
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
