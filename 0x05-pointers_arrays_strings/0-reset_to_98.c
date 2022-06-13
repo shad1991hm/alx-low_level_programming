3f9e455095084f06bbc72326b19620eb49866d60
Sidrak <shad1991hm@gmail.com>
2022-06-12 22:20:59 -0700

    $message

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the value of n to number 98
+ * @n: the number to be reset
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Readme file for 0x05 pointers arrays and strings
