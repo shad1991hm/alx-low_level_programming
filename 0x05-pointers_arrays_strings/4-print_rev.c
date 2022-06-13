0fa15cc32444cf0f521a0ac61238f9533d01a4ed
Sidrak <shad1991hm@gmail.com>
2022-06-13 04:14:49 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,9 +8,9 @@ void print_rev(char *s)
 {
 	int len = 0, i;
 
-	while (*s++)
+	while (s[i] != \0)
 		len++;
-	for (i = len - 1; i >= 0; i--)
+	for (i = len; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
