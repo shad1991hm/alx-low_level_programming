f0d6b20ffdfe8a7a78e65dca220a4bb48c6f2628
Sidrak <shad1991hm@gmail.com>
2022-06-22 19:28:07 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Function that returns the factorial of a given number
+ * @x: Base
+ * @y: Power
+ * Return: Result of x raised to the power of y.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	return (x * _pow_recursion(x, y - 1));
+}
