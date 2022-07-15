16d043c953b6d0cc03b549c15e676b79e10e3b53
Sidrak <36000028+shad1991hm@users.noreply.github.com>
2022-07-15 05:43:06 +0300

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-
+int _strlen(const char *s);
 /**
   * add_node - Adds a new node at the beginning of a list
   * @head: The original linked list
