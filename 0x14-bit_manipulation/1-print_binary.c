985cd9352371939f4125b6277a80c65c997a2af8
Sidrak <shad1991hm@gmail.com>
2022-07-21 09:42:33 -0700

    Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_binary - prints binary
+ * @n: decimal number to to convert
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp;
+	int a;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	temp = n;
+	a = 0;
+	while ((temp >>= 1) > 0)
+		a++;
+	while (a >= 0)
+	{
+		if ((n >> a) & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+		a--;
+	}
+}
