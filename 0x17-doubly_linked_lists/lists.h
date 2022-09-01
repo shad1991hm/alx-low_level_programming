292df487a2c575adb0225895ff4760f1f81e254e
Sidrak <36000028+shad1991hm@users.noreply.github.com>
2022-09-01 22:54:58 +0300

    Lists

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -27,6 +27,8 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+dlistint_t *create_node(unsigned int n, void *next, void *prev);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+int delete_first_dnode(dlistint_t **head);
 
 #endif /* _LISTS_H_ */
