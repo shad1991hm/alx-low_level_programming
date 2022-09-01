38bdae7a3465dc6e436a48de912d30ee35617fd9
Sidrak <shad1991hm@gmail.com>
2022-09-01 13:46:06 -0700

    Task 0

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,7 +4,6 @@
 /**
  * dlistint_len - count the number of nodes in the linked list
  * @h: pointer to the beginning of a linked list
- *
  * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
